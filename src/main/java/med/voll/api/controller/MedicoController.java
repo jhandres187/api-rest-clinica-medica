f24e2bbf944b16aba7e23cbd348d0ab1e0784f55
jhonny andres ibañez garzon <jhandres187@gmail.com>
2023-10-03 17:36:12 -0500

    subida archivos

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,48 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+    @Autowired//no recomendable para testing para fines didacticos no para la practica
+    private MedicoRepository medicoRepository;
+    @PostMapping
+    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico){
+        medicoRepository.save(new Medico(datosRegistroMedico));
+    }
+    @GetMapping
+    public Page<DatosListadoMedico> listadoMedicos(@PageableDefault(size = 2) Pageable paginacion){
+        return medicoRepository.findByActivoTrue(paginacion).map(DatosListadoMedico::new);
+        //return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
+    }
+    @PutMapping
+    @Transactional//abre y despues de la transaccion termina la tr
+    public void actualizarMedico(@RequestBody @Valid DatosActualizarMedico datosActualizarMedico){
+        Medico medico = medicoRepository.getReferenceById(datosActualizarMedico.id());
+        medico.actualizarDatos(datosActualizarMedico);
+    }
+    //delete completo
+    //@DeleteMapping("/{id}")
+    //@Transactional
+    //public void eliminarMedico(@PathVariable Long id){
+        //Medico medico = medicoRepository.getReferenceById(id);
+        //medicoRepository.delete(medico);
+    //}
+    //delete logico
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void eliminarMedico(@PathVariable Long id){
+        Medico medico = medicoRepository.getReferenceById(id);
+        medico.desactivarMedico();
+    }
+}
